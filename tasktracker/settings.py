2757dc46e5ff7c07154835e8d675c146583d495f
Vitalii Hubenko <gubenko23r@gmail.com>
2026-03-04 17:48:09 +0200

    feat(templates): create base.html, include Bootstrap and configure custom styles

--- tasktracker/settings.py
+++ tasktracker/settings.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = "tasktracker.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
