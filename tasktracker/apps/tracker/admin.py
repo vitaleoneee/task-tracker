ade0b1007f4520fb0ce91450fe1434ffe3d51af6
Vitalii Hubenko <gubenko23r@gmail.com>
2026-03-05 10:36:37 +0200

    feat(admin): register Project and Task models in the admin panel

--- tasktracker/apps/tracker/admin.py
+++ tasktracker/apps/tracker/admin.py
@@ -1,3 +1,19 @@
-# from django.contrib import admin
+from django.contrib import admin
+from tasktracker.apps.tracker.models import (
+    Project,
+    Task,
+)
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "created_at")
+    search_fields = ("name", "owner__username")
+    list_filter = ("created_at",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "project", "is_completed", "priority", "due_date")
+    search_fields = ("title", "project__name")
+    list_filter = ("is_completed", "priority", "due_date")
