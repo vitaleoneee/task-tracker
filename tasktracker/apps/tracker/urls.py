1ddd8b1dd9faf8bb8e19cab6857d9cbe804149f3
Vitalii Hubenko <gubenko23r@gmail.com>
2026-03-06 07:50:01 +0200

    feat(list): update project list after creating a project (htmx)

--- tasktracker/apps/tracker/urls.py
+++ tasktracker/apps/tracker/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
 from tasktracker.apps.tracker.views.common_views import DashboardView
-from tasktracker.apps.tracker.views.projects.project_views import ProjectCreateView
+from tasktracker.apps.tracker.views.projects.project_views import (
+    ProjectCreateView,
+    ProjectListView,
+)
 
 app_name = "tracker"
 urlpatterns = [
     path("", DashboardView.as_view(), name="dashboard"),
+    # Projects urls
     path("add-project/", ProjectCreateView.as_view(), name="add_project"),
+    path("project-list/", ProjectListView.as_view(), name="project_list"),
 ]
--- tasktracker/apps/tracker/views/projects/project_views.py
+++ tasktracker/apps/tracker/views/projects/project_views.py
@@ -1,5 +1,7 @@
+import json
+
 from django.http import HttpResponse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 from tasktracker.apps.tracker.models import Project
 
@@ -8,12 +10,20 @@ class ProjectCreateView(CreateView):
     model = Project
     template_name = "tracker/partials/projects/create_project.html"
     fields = ["name"]
-    success_url = "tracker:dashboard"
 
     def form_valid(self, form):
         form.instance.owner = self.request.user
         self.object = form.save()
         response = HttpResponse()
-        # Return an HTMX response to trigger the modal close
-        response["HX-Trigger"] = "closeModal"
+        # Return an HTMX response to trigger the modal close and update the project list
+        response["HX-Trigger"] = json.dumps({"closeModal": None, "refreshData": None})
         return response
+
+
+class ProjectListView(ListView):
+    model = Project
+    template_name = "tracker/partials/projects/project_list.html"
+    context_object_name = "projects"
+
+    def get_queryset(self):
+        return Project.objects.filter(owner=self.request.user)
