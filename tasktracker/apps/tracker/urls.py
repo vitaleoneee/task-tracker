9f0b67512dea3441fe53d6d9a08c9fb617a73d5f
Vitalii Hubenko <gubenko23r@gmail.com>
2026-03-06 08:11:41 +0200

    feat(delete): delete project using htmx

--- tasktracker/apps/tracker/urls.py
+++ tasktracker/apps/tracker/urls.py
@@ -4,6 +4,7 @@ from tasktracker.apps.tracker.views.common_views import DashboardView
 from tasktracker.apps.tracker.views.projects.project_views import (
     ProjectCreateView,
     ProjectListView,
+    ProjectDeleteView,
 )
 
 app_name = "tracker"
@@ -12,4 +13,7 @@ urlpatterns = [
     # Projects urls
     path("add-project/", ProjectCreateView.as_view(), name="add_project"),
     path("project-list/", ProjectListView.as_view(), name="project_list"),
+    path(
+        "project-delete/<int:pk>/", ProjectDeleteView.as_view(), name="project_delete"
+    ),
 ]
--- tasktracker/apps/tracker/views/projects/project_views.py
+++ tasktracker/apps/tracker/views/projects/project_views.py
@@ -1,9 +1,18 @@
 import json
 
 from django.http import HttpResponse
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DeleteView
 
 from tasktracker.apps.tracker.models import Project
+
+
+class ProjectListView(ListView):
+    model = Project
+    template_name = "tracker/partials/projects/project_list.html"
+    context_object_name = "projects"
+
+    def get_queryset(self):
+        return Project.objects.filter(owner=self.request.user)
 
 
 class ProjectCreateView(CreateView):
@@ -16,14 +25,17 @@ class ProjectCreateView(CreateView):
         self.object = form.save()
         response = HttpResponse()
         # Return an HTMX response to trigger the modal close and update the project list
-        response["HX-Trigger"] = json.dumps({"closeModal": None, "refreshData": None})
+        response["HX-Trigger"] = json.dumps({"closeModal": True, "refreshData": True})
         return response
 
 
-class ProjectListView(ListView):
+class ProjectDeleteView(DeleteView):
     model = Project
-    template_name = "tracker/partials/projects/project_list.html"
-    context_object_name = "projects"
 
-    def get_queryset(self):
-        return Project.objects.filter(owner=self.request.user)
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.delete()
+        response = HttpResponse()
+        # Return an HTMX response to trigger the project list refresh
+        response["HX-Trigger"] = "refreshData"
+        return response
